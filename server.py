56d88de1f25a9cbc96d8d40e776d2102a4d3182a
Adam Snyder <asnyder@ea.com>
2016-11-26 19:51:37 -0800

    pip requirements, server cleans up on exit

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 import SocketServer
+import atexit
 
 import handler
 import log
 
 PORT = 8032
-httpd = SocketServer.TCPServer(("", PORT), handler.Handler)
 
 
 def main():
+    # define a server that runs our handler
+    httpd = SocketServer.TCPServer(("", PORT), handler.Handler)
+    # register the server shutdown function with atexit so it cleans up when the script is exited
+    atexit.register(httpd.shutdown)
     log.info("serving at port " + str(PORT))
+    # start the server
     httpd.serve_forever()
 
 
