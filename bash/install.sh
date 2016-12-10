d2c5e96f81cab359903bb27edf12354c9f39209d
Adam Snyder <armsnyder@gmail.com>
2016-12-10 13:45:11 -0800

    restrict daemon file permissions

--- bash/install.sh
+++ bash/install.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+# Check that we are superuser (i.e. $(id -u) is zero)
+if (( $(id -u) ))
+then
+    echo "This script needs to run as root"
+    exit 1
+fi
+
 # define function to exit script early if it detects a failure
 catch() { [[ $? -eq 0 ]] || exit 1; }
 
 install_path="/opt/armsnyder/smarthome"
+log_path="/var/armsnyder/smarthome/"
 daemon_path="/Library/LaunchDaemons"
 daemon_pattern="com.armsnyder.smarthome.*"
 remote_repo="https://github.com/armsnyder/smart-home.git"
+username=_smarthome
 
 # create install directory
 mkdir -p "${install_path}"
@@ -39,6 +48,20 @@ catch
 # generate ssl certificate
 mkdir python/ssl
 openssl req -batch -nodes -x509 -newkey rsa:2048 -keyout python/ssl/key.pem -out python/ssl/cert.pem -days 365
+
+# create daemon user account
+dscl . -create /Groups/${username}
+dscl . -append /Groups/${username} PrimaryGroupID 332
+dscl . -create /Users/${username}
+dscl . -append /Users/${username} UniqueID 332
+dscl . -append /Users/${username} PrimaryGroupID 332
+
+# restrict files to daemon
+chown -R ${username}:${username} ${install_path}
+
+# grant permission to log files
+mkdir -p "${log_path}" 2>/dev/null
+chown -R ${username}:${username} ${log_path}
 
 # load the launchd daemons to start the service
 find "${daemon_path}" -name "${daemon_pattern}" -exec sudo launchctl load -w {} \;
--- bash/uninstall.sh
+++ bash/uninstall.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# Check that we are superuser (i.e. $(id -u) is zero)
+if (( $(id -u) ))
+then
+    echo "This script needs to run as root"
+    exit 1
+fi
+
 # define function to exit script early if it detects a failure
 catch() { [[ $? -eq 0 ]] || exit 1; }
 
 install_path="/opt/armsnyder/smarthome"
 daemon_path="/Library/LaunchDaemons"
 daemon_pattern="com.armsnyder.smarthome.*"
+username=_smarthome
 
 # unload daemons
 find "${daemon_path}" -name "${daemon_pattern}" -exec sudo launchctl unload {} \;
@@ -20,5 +28,9 @@ if [ -d "${install_path}" ]; then
     rm -R "${install_path}"
     catch
 fi
+
+# delete daemon user account
+dscl . -delete /Groups/${username}
+dscl . -delete /Users/${username}
 
 echo "Finished uninstalling"
